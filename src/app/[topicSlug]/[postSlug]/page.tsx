20b6f5826d1035979282d8a708b27658671ed9d3
huiwu Zhang <pizza6inch@gmail.com>
2025-04-23 15:42:10 +0800

    arrow animation

--- src/app/[topicSlug]/[postSlug]/page.tsx
+++ src/app/[topicSlug]/[postSlug]/page.tsx
@@ -11,6 +11,7 @@ import BreadcrumbLinks from "@/components/BreadcrumbLinks";
 import Link from "next/link";
 import { title } from "process";
 import { formatDate } from "@/lib/utils";
+import { ArrowLeft, ArrowRight } from "lucide-react";
 
 export const dynamicParams = false;
 
@@ -40,6 +41,21 @@ export default async function Page({ params }: { params: Promise<{ topicSlug: st
 
   console.log(postDetail);
   console.log(categoryPosts);
+
+  const findPrevNextPosts = (postSlug: string) => {
+    const allPosts = categoryPosts.flatMap((category) => category.posts);
+
+    const currentIndex = allPosts.findIndex((post) => post.slug === postSlug);
+
+    if (currentIndex === -1) return { prev: null, next: null }; // 沒找到
+
+    const prev = currentIndex > 0 ? allPosts[currentIndex - 1] : null;
+    const next = currentIndex < allPosts.length - 1 ? allPosts[currentIndex + 1] : null;
+
+    return { prev, next };
+  };
+
+  const { prev: prevPost, next: nextPost } = findPrevNextPosts(postDetail.slug || "");
 
   const testCategoryPosts = [
     {
@@ -80,52 +96,54 @@ export default async function Page({ params }: { params: Promise<{ topicSlug: st
       title: topic?.title || "error",
       links: `/${topicSlug}`,
     },
-    {
-      title: postDetail?.categoryTitle || "error",
-      links: `/${topicSlug}`,
-    },
+    // {
+    //   title: postDetail?.categoryTitle || "error",
+    //   links: `/${topicSlug}`,
+    // },
     {
       title: postDetail?.title || "error",
       links: `/${topicSlug}/${postDetail.slug}`,
     },
   ];
 
-  // const categoryPosts =
-
-  // 寫query fetch post detail
-
-  // 寫query fetch 同個category下的post 按照subCategory分類
-
   return (
-    <MainLayout>
-      <div className="md:block hidden fixed w-60 overflow-y-auto h-[90vh] py-10 px-5 bg-gray-100 dark:bg-gray-900 custom-scrollbar ">
-        <nav className="p-5 bg-gray-100 dark:bg-gray-900 rounded-lg shadow-md dark:shadow-gray-700">
-          <h2 className="text-2xl font-bold mb-4 border-b-2 text-foreground border-gray-300 dark:border-gray-700 pb-2">
-            目錄
-          </h2>
-          <ul className="space-y-5">
-            {categoryPosts.map((category) => (
-              <li key={category.title}>
-                <h3 className="text-lg font-semibold text-foreground mb-2 ">{category.title}</h3>
-                <ul className="pl-4 space-y-2">
-                  {category.posts.map((post) => (
-                    <li key={post.title} className="group">
-                      <Link href={`/${topicSlug}/${post.slug}`}>
-                        <p className="text-gray-600 dark:text-gray-400 hover:text-gray-900 hover:dark:text-gray-200 cursor-pointer transition-colors relative">
-                          {post.title}
-                          <span className="absolute right-0 bottom-0 w-0 h-[2px] bg-primary transition-all duration-500 group-hover:w-full group-hover:left-0"></span>
-                        </p>
-                      </Link>
-                    </li>
-                  ))}
-                </ul>
-              </li>
-            ))}
-          </ul>
-        </nav>
-      </div>
-      <div className=" grid grid-cols-12 pl-0 md:pl-60">
-        <div className=" col-span-12 lg:col-span-9 py-10 px-10 space-y-5">
+    <>
+      <div className="flex">
+        <div className="md:block hidden sticky top-[10vh] w-60 overflow-y-auto h-[90vh] py-10 px-5 bg-gray-100 dark:bg-gray-900 custom-scrollbar ">
+          <nav className="p-5 bg-gray-100 dark:bg-gray-900 rounded-lg shadow-md dark:shadow-gray-700">
+            <h2 className="text-2xl font-bold mb-4 border-b-2 text-foreground border-gray-300 dark:border-gray-700 pb-2">
+              目錄
+            </h2>
+            <ul className="space-y-5">
+              {categoryPosts.map((category) => (
+                <li key={category.title}>
+                  <h3 className="text-lg font-semibold text-foreground mb-2 ">{category.title}</h3>
+                  <ul className="pl-4 space-y-2">
+                    {category.posts.map((post) => (
+                      <li key={post.title} className="group">
+                        <Link href={`/${topicSlug}/${post.slug}`}>
+                          {post.slug === postDetail.slug ? (
+                            <p className="text-primary relative  pb-1">
+                              {post.title}
+                              <span className="h-[2px] bg-primary absolute right-0 bottom-0 w-full"></span>
+                            </p>
+                          ) : (
+                            <p className="text-gray-600 dark:text-gray-400 hover:text-gray-900 hover:dark:text-gray-200 cursor-pointer transition-colors relative pb-1">
+                              {post.title}
+                              <span className="absolute right-0 bottom-0 w-0 h-[2px] bg-primary transition-all duration-500 group-hover:w-full group-hover:left-0"></span>
+                            </p>
+                          )}
+                        </Link>
+                      </li>
+                    ))}
+                  </ul>
+                </li>
+              ))}
+            </ul>
+          </nav>
+        </div>
+
+        <div className=" py-10 px-5 md:px-10 space-y-10 w-full">
           <BreadcrumbLinks items={breadcrumbItems} />
           <div className=" space-y-1">
             <h1 className="text-4xl">{postDetail.title}</h1>
@@ -133,10 +151,45 @@ export default async function Page({ params }: { params: Promise<{ topicSlug: st
           </div>
           {postDetail.description && <MarkdownBlock content={postDetail.description} />}
           {postDetail.content && <MarkdownBlock content={postDetail.content} />}
+          {/* next post & prev post */}
+          <hr />
+          <div className="flex justify-between items-center mt-10">
+            {prevPost ? (
+              <Link
+                href={`/${topicSlug}/${prevPost.slug}`}
+                className="group text-sm flex flex-col items-start text-gray-600 dark:text-gray-400 hover:text-gray-900 hover:dark:text-gray-200 cursor-pointer transition-colors"
+              >
+                <p className="mb-2 flex items-center gap-1 arrow">
+                  <ArrowLeft className="w-4 h-4" />
+                  上一篇
+                </p>
+                <span>{prevPost.title}</span>
+              </Link>
+            ) : (
+              <div />
+            )}
+
+            {nextPost ? (
+              <Link
+                href={`/${topicSlug}/${nextPost.slug}`}
+                className="group text-sm flex flex-col items-end text-gray-600 dark:text-gray-400 hover:text-gray-900 hover:dark:text-gray-200 cursor-pointer transition-colors"
+              >
+                <p className="mb-2 flex items-center gap-1 arrow">
+                  下一篇
+                  <ArrowRight className="w-4 h-4" />
+                </p>
+                <span>{nextPost.title}</span>
+              </Link>
+            ) : (
+              <div />
+            )}
+          </div>
         </div>
-        <div className="lg:block hidden col-span-3 ">Right </div>
       </div>
-    </MainLayout>
+
+      {/* comment */}
+      <div className="h-60 bg-black">comment section</div>
+    </>
   );
 }
 
--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -5,6 +5,8 @@ import ClientBody from "./ClientBody";
 import { cn } from "@/lib/utils";
 import { ThemeProvider } from "@/components/ThemeProvider";
 import { SanityLive } from "@/sanity/lib/live";
+import Header from "@/components/Header";
+import Footer from "@/components/Footer";
 import "easymde/dist/easymde.min.css";
 
 // Import Noto Sans TC for traditional Chinese
@@ -31,7 +33,11 @@ export default function RootLayout({
     <html lang="zh-TW" className={cn(raleway.variable, notoSansTC.variable)}>
       <ThemeProvider defaultTheme="light">
         <ClientBody>
-          {children}
+          <div className="flex flex-col min-h-screen">
+            <Header />
+            <main className="flex-grow pt-16">{children}</main>
+            <Footer />
+          </div>
           <SanityLive />
         </ClientBody>
       </ThemeProvider>
