0053d2ae89ea76ee8910873a1f72107287379e46
huiwu Zhang <pizza6inch@gmail.com>
2025-06-10 14:16:35 +0800

    fix: markdown style

--- src/components/MarkdownBlock.tsx
+++ src/components/MarkdownBlock.tsx
@@ -1,15 +1,57 @@
 import ReactMarkdown from "react-markdown";
+import Markdown from "react-markdown";
 
 import remarkGfm from "remark-gfm";
 import rehypeHighlight from "rehype-highlight";
+import { Prism as SyntaxHighlighter } from "react-syntax-highlighter";
+import { dark } from "react-syntax-highlighter/dist/esm/styles/prism";
 import "github-markdown-css/github-markdown-dark.css";
 
 export default function MarkdownBlock({ content }: Readonly<{ content: string }>) {
+  const test = `A paragraph with *emphasis* and **strong importance**.
+
+> A block quote with ~strikethrough~ and a URL: https://reactjs.org.
+
+* Lists
+* [ ] todo
+* [x] done
+
+A table:
+
+| a | b |
+| - | - |
+`;
+
   return (
-    <div className="markdown-body ">
-      <ReactMarkdown remarkPlugins={[remarkGfm]} rehypePlugins={[rehypeHighlight]}>
-        {content}
-      </ReactMarkdown>
+    // <div className="markdown-body ">
+    //   <ReactMarkdown remarkPlugins={[remarkGfm]} rehypePlugins={[rehypeHighlight]}>
+    //     {markdown}
+    //   </ReactMarkdown>
+    // </div>
+    <div className="markdown-body">
+      <ReactMarkdown
+        children={content}
+        remarkPlugins={[remarkGfm]}
+        components={{
+          code(props) {
+            const { children, className, node, ...rest } = props;
+            const match = /language-(\w+)/.exec(className || "");
+            return match ? (
+              <SyntaxHighlighter
+                {...rest}
+                PreTag="div"
+                children={String(children).replace(/\n$/, "")}
+                language={match[1]}
+                style={dark}
+              />
+            ) : (
+              <code {...rest} className={className}>
+                {children}
+              </code>
+            );
+          },
+        }}
+      />
     </div>
   );
 }
